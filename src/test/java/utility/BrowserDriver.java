5f0fce8133703c9011af7513eb6c74df6ed3d928
Prashant <tl.prashant@gmail.com>
2025-08-11 20:24:01 +0530

    SeleniumCucumberWebDriverManager commit Add Jenkins headless mode in BrowserDriver

--- src/test/java/utility/BrowserDriver.java
+++ src/test/java/utility/BrowserDriver.java
@@ -3,6 +3,7 @@ package utility;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BrowserDriver {
     public static WebDriver driver;
@@ -10,11 +11,25 @@ public class BrowserDriver {
     public static void initializeDriver() {
         if (driver == null) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            driver.manage().window().maximize(); // only once
+
+            ChromeOptions options = new ChromeOptions();
+
+            // Detect if running in Jenkins (no DISPLAY variable or CI env)
+            String ciEnv = System.getenv("JENKINS_HOME");
+            if (ciEnv != null) {
+                options.addArguments("--headless=new"); // Use headless mode in Jenkins
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+            }
+
+            driver = new ChromeDriver(options);
+            driver.manage().window().maximize();
         }
     }
 }
+
 
 
 
